7990b0893b870a2c5d07aea3db11c0d93ad41036
FranciscoJavier <fjpizarro@localhost.localdomain>
2022-03-08 21:52:20 +0100

    Web Scrapping hecho(revisar estadios)

--- scripts/estadioScrapper.py
+++ scripts/estadioScrapper.py
@@ -1,20 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-def buscarUrlEstadio(urlEquipo):
-    """
-    Dada la url de un equipo en la wikipedia, devuelve la url en wikipedia correspondiente
-    al estadio de dicho equipo
-    """
-    html = requests.get(urlEquipo).text
-    soup = BeautifulSoup(html, 'html.parser')
-    table = soup.table
-    rows = table.find_all("tr")
-    for row in rows:
-        if "Estadio" in row.get_text():
-            links = row.a
-            link = links.get("href")
-            return "https://es.wikipedia.org" + link
+# def buscarUrlEstadio(urlEquipo):
+#     """
+#     Dada la url de un equipo en la wikipedia, devuelve la url en wikipedia correspondiente
+#     al estadio de dicho equipo
+#     """
+#     html = requests.get(urlEquipo).text
+#     soup = BeautifulSoup(html, 'html.parser')
+#     table = soup.table
+#     rows = table.find_all("tr")
+#     for row in rows:
+#         if "Estadio" in row.get_text():
+#             links = row.a
+#             link = links.get("href")
+#             return "https://es.wikipedia.org" + link
 
 def datosEstadio(urlEstadio):
     """
@@ -36,20 +36,48 @@ def datosEstadio(urlEstadio):
             cap = row.td.get_text()
             cap = cap.replace(" ", "")
             cap = cap.replace(" ", "")
-            cap = cap[:cap.find("e") - 1]
+            cap = cap.replace(".", "")
+            cap = cap[:cap.find("[")]
+            cap = cap[:cap.find("e")]
             break
     for row in rows:
         if "Apertura" in row.get_text():
             aper = row.td.get_text()
             break
     cap = cap[cap.find("\n") + 1:]
-    cap = int(cap)
     aper = aper[aper.find("\n") + 1:]
     return {"nombre":nombre,"capacidad":cap,"apertura":aper}
 
-urlEquipo = "https://es.wikipedia.org/wiki/Real_Zaragoza"
-urlEstadio = buscarUrlEstadio(urlEquipo)
-datos = datosEstadio(urlEstadio)
-print(datos["nombre"])
-print(datos["capacidad"])
-print(datos["apertura"])
+def cargarUrls():
+    """
+    Carga las urls de los estadios desde el fichero de datos de los equipos
+    Devuelve una lista con las urls
+    """
+    urls = []
+    f = open("dataEquipos.csv", "r", encoding="utf-8")
+    for linea in f:
+        campos = linea.split(";")
+        campos[7] = campos[7].replace("\n","")
+        if campos[7] not in urls:
+            urls.append(campos[7])
+    f.close()
+    return urls
+
+def guardarDatos(urls):
+    """
+    Dada una lista de urls de estadios hace webscrapping y almacena los datos en dataEstadios.csv
+
+    NOMBRE_ESTADIO;CAPACIDAD;FECHA_DE_APERTURA
+    """
+    f = open("dataEstadios.csv", "w", encoding="utf-8")
+    for urlEstadio in urls:
+        try:
+            datos = datosEstadio(urlEstadio)
+            f.write(datos["nombre"] + ";" + datos["capacidad"] + ";" + datos["apertura"] + "\n")
+        except:
+            print("Falla:",urlEstadio)
+            f.write("¿¿¿???" + ";" + "¿¿¿???" + ";" + "¿¿¿???" + "\n")
+    f.close()
+
+urls = cargarUrls()
+guardarDatos(urls)
