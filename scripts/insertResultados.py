0ecbbc38d4dcc7e59c610859db3ba660771fd36b
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-03-23 20:55:21 +0100

    okay

--- scripts/insertResultados.py
+++ scripts/insertResultados.py
@@ -10,5 +10,5 @@ with open(SQL_FOLDER + filename + ".sql", "w", encoding="utf-8") as fd:
     prefijo = "INSERT INTO resultados (puntos, puesto, golesAF, golesEC, partidosGanados, partidosEmpatados, partidosPerdidos, asciende, desciende, europa, equipo, division, temporada, numJornada) values "
 
     for index, row in df.iterrows():
-        values = "({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, '{}', '{}', {}, {})".format(row["puntos"], row["puesto"], row["golesAF"],row["golesEC"],row["partidosGanados"],row["partidosEmpatados"],row["partidosPerdidos"],row["asciende"],row["desciende"],row["europa"],row["equipo"],row["division"],row["temporada"],row["numJornada"])
+        values = "({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, '{}', '{}', {}, {});".format(row["puntos"], row["puesto"], row["golesAF"],row["golesEC"],row["partidosGanados"],row["partidosEmpatados"],row["partidosPerdidos"],row["asciende"],row["desciende"],row["europa"],row["equipo"],row["division"],row["temporada"],row["numJornada"])
         fd.write(prefijo + values + "\n")
